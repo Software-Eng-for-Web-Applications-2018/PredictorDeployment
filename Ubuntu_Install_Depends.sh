2a6604c4a6986517563d9379f205b1d0a78652a8
John Grun <john.grun@gmail.com>
2018-04-23 21:59:34 -0400

    Updated the script to correct issues

--- Ubuntu_Install_Depends.sh
+++ Ubuntu_Install_Depends.sh
@@ -3,7 +3,8 @@
 sudo apt-get update && sudo apt-get install -y build-essential curl libcurl3-dev git libfreetype6-dev libpng12-dev libzmq3-dev pkg-config python-dev python-numpy python-pip software-properties-common swig zip zlib1g-dev
 sudo pip install grpcio
 
-sudo pip install tensorflow-serving-api
+#sudo pip install tensorflow-serving-api
+sudo -H pip install tensorflow-serving-api-python3
 
 #Copy over files to be able to run the code in python 3.5. Very stupid
 sudo cp -r /usr/local/lib/python2.7/dist-packages/tensorflow_serving /usr/local/lib/python3.5/dist-packages/tensorflow_serving 
@@ -18,6 +19,8 @@ sudo apt update
 
 sudo apt-get install libstdc++6
 
-wget http://storage.googleapis.com/tensorflow-serving-apt/pool/tensorflow-model-server-1.5.0/t/tensorflow-model-server/tensorflow-model-server_1.5.0_all.deb
-sudo dpkg -i tensorflow-model-server_1.5.0_all.deb
-#sudo apt-get install tensorflow-model-server
+#wget http://storage.googleapis.com/tensorflow-serving-apt/pool/tensorflow-model-server-1.5.0/t/tensorflow-model-server/tensorflow-model-server_1.5.0_all.deb
+#sudo dpkg -i tensorflow-model-server_1.5.0_all.deb
+sudo apt-get install tensorflow-model-server
+
+
